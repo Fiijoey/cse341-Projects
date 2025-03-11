703b0a73be92d8ecf9ae2634d5f5e51eb4c320cd
Fiijoey <kofimensah3730@gmail.com>
2025-03-11 15:38:44 +0000

    Refactor user routes and controller to contacts; implement new contacts controller and update routing

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const usersController = require("../controllers/users");
+const usersController = require("../controllers/contactss");
 
 router.get("/", usersController.getAll);
 
